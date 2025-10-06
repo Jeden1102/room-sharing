c255be0eba85074d6c007e63fd31435ef24a4e01
Jeden1102 <bum123@o2.pl>
2025-10-06 20:23:38 +0200

    master. privacy/terms bluebriints pages.

---
+++ app/pages/privacy-policy.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="container flex flex-col gap-6 py-6">
+    <AppHeading> Privacy policty </AppHeading>
+  </main>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  auth: false,
+});
+
+usePageSeo({
+  title: "Privacy policy",
+  description: "Privacy policy",
+});
+</script>
---
+++ app/pages/terms.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="container flex flex-col gap-6 py-6">
+    <AppHeading> Terms </AppHeading>
+  </main>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  auth: false,
+});
+
+usePageSeo({
+  title: "Terms",
+  description: "Terms",
+});
+</script>
