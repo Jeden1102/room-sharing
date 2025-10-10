fc86ac3151664b0e0d94b27fdfb2fc88263efa14
Jeden1102 <bum123@o2.pl>
2025-10-11 01:25:23 +0200

    master. Nitro caching test.

--- server/api/user/taxonomies.ts
+++ server/api/user/taxonomies.ts
@@ -1,26 +1,29 @@
-export default defineEventHandler(async (event) => {
-  const [
-    interests,
-    occupations,
-    searchPreferences,
-    propertyTypes,
-    noiseCompatibility,
-    petsCompatibility,
-  ] = await Promise.all([
-    $fetch("/api/taxonomy_term/interest"),
-    $fetch("/api/taxonomy_term/occupation"),
-    $fetch("/api/taxonomy_term/searchPreference"),
-    $fetch("/api/taxonomy_term/propertyOption"),
-    $fetch("/api/taxonomy_term/noiseCompatibility"),
-    $fetch("/api/taxonomy_term/PetsCompatibility"),
-  ]);
+export default defineCachedEventHandler(
+  async (event) => {
+    const [
+      interests,
+      occupations,
+      searchPreferences,
+      propertyTypes,
+      noiseCompatibility,
+      petsCompatibility,
+    ] = await Promise.all([
+      $fetch("/api/taxonomy_term/interest"),
+      $fetch("/api/taxonomy_term/occupation"),
+      $fetch("/api/taxonomy_term/searchPreference"),
+      $fetch("/api/taxonomy_term/propertyOption"),
+      $fetch("/api/taxonomy_term/noiseCompatibility"),
+      $fetch("/api/taxonomy_term/PetsCompatibility"),
+    ]);
 
-  return {
-    interests,
-    occupations,
-    searchPreferences,
-    propertyTypes,
-    noiseCompatibility,
-    petsCompatibility,
-  };
-});
+    return {
+      interests,
+      occupations,
+      searchPreferences,
+      propertyTypes,
+      noiseCompatibility,
+      petsCompatibility,
+    };
+  },
+  { maxAge: 60 * 60 },
+);
