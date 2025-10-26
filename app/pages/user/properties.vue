10c562e09cd36274577f8cc3814b504c58b45bea
Jeden1102 <bum123@o2.pl>
2025-10-26 16:15:39 +0100

    master. Properties listing row.

--- app/pages/user/properties.vue
+++ app/pages/user/properties.vue
@@ -4,8 +4,19 @@
       v-if="data.properties"
       :value="data.properties"
       tableStyle="min-width: 50rem"
+      paginator
+      stripedRows
+      :rows="10"
+      v-model:editingRows="editingRows"
+      editMode="row"
+      dataKey="id"
+      @row-edit-save="onRowEditSave"
     >
-      <Column field="title" header="Title" sortable style="width: 25%"></Column>
+      <Column field="title" header="Title" sortable style="width: 25%">
+        <template #editor="{ data, field }">
+          <InputText v-model="data[field]" fluid />
+        </template>
+      </Column>
       <Column field="city" header="City" sortable style="width: 25%"></Column>
       <Column field="price" header="Price" sortable style="width: 25%"></Column>
       <Column
@@ -16,21 +27,29 @@
       ></Column>
       <Column header="Actions">
         <template #body="slotProps">
-          <RouterLink
-            :to="
-              $localePath({
-                name: 'properties-id',
-                params: {
-                  id: slugify(slotProps.data.title),
-                },
-                query: { id: slotProps.data.id },
-              })
-            "
-          >
-            Podgląd
-          </RouterLink>
+          <Button asChild v-slot="buttonProps">
+            <RouterLink
+              :class="buttonProps.class"
+              :to="
+                $localePath({
+                  name: 'properties-id',
+                  params: {
+                    id: slugify(slotProps.data.title),
+                  },
+                  query: { id: slotProps.data.id },
+                })
+              "
+            >
+              Podgląd
+            </RouterLink>
+          </Button>
         </template>
       </Column>
+      <Column
+        :rowEditor="true"
+        style="width: 10%; min-width: 8rem"
+        bodyStyle="text-align:center"
+      ></Column>
     </DataTable>
   </ClientOnly>
 </template>
@@ -40,6 +59,14 @@ usePageSeo({
   title: "Properties",
   description: "Properties list",
 });
+
+const editingRows = ref([]);
+
+const onRowEditSave = (event) => {
+  let { newData, index } = event;
+
+  console.log(newData, index);
+};
 
 const { status, data } = await useFetch("/api/properties/my", { lazy: true });
 </script>
