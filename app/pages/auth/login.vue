85363ff809f65f005cb4086ce45ddf2f86175b85
Jeden1102 <bum123@o2.pl>
2025-11-27 21:28:20 +0100

    master. Features redirect info added.

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <Message v-if="Object.keys(route.query).length" class="mb-8">
+      {{ mapFeatureInfo }}
+    </Message>
     <h1 class="text-3xl font-semibold">{{ $t("loginPage.title") }}</h1>
     <p class="mt-2 mb-6 font-light">
       {{ $t("loginPage.subtitle") }}
@@ -9,6 +12,8 @@
 </template>
 
 <script setup lang="ts">
+const route = useRoute();
+
 definePageMeta({
   layout: "login",
   unauthenticatedOnly: true,
@@ -19,5 +24,13 @@ const { t } = useI18n();
 usePageSeo({
   title: t("seo.login.title"),
   description: t("seo.login.description"),
+});
+
+const mapFeatureInfo = computed(() => {
+  const featuresMessage = {
+    bookmarks: t("loginPage.feature.bookmarks"),
+  };
+
+  return featuresMessage[route.query.feat as keyof typeof featuresMessage];
 });
 </script>
